6a22bbe4c789d0db416b97d9c1f5ebcd045c2d3c
betester <ikramzulkarnaen02@gmail.com>
2022-08-26 20:05:37 +0700

    fix(database): redis configuration

--- database/index.js
+++ database/index.js
@@ -6,7 +6,11 @@ const redis_password = process.env.REDIS_PASSWORD;
 const redis_username = process.env.REDIS_USERNAME;
 
 
-const client = createClient();
+const client = createClient({
+  url : redis_url,
+  password : redis_password,
+  username : redis_username
+});
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 client.connect().then(() => {
