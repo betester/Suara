87991759499af4e990e6a0f4c3eab245a5ac7201
betester <ikramzulkarnaen02@gmail.com>
2022-08-26 20:02:07 +0700

    fix: add mutex on fetching data to handle race condition

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,3 +1,5 @@
+var Mutex = require("async-mutex").Mutex;
+
 const fetchChannel = (client, channelId, args, callback) => {
   if (channelId == null) return;
   return client.channels
@@ -21,6 +23,47 @@ const getSetOfUsernameFromVC = (channel) => {
 
   return set;
 };
+
+const fetchRedisData = async (client,oldState,newState) => {
+  const { redisClient } = client;
+  const mutex = new Mutex();
+  let newStateData;
+  let oldStateData;
+
+  await mutex.runExclusive(async () => {
+    const redisNewStateChannelUsers = await redisClient.SMEMBERS(
+      `${newState.guild.id}:${newState.channelId}`
+    );
+
+    const redisOldStateChannelUsers = await redisClient.SMEMBERS(
+      `${oldState.guild.id}:${oldState.channelId}`
+    );
+
+    newStateData = await fetchChannel(
+      client,
+      newState.channelId,
+      {
+        redisClient: redisClient,
+        channelUsers: redisNewStateChannelUsers,
+        guildId: newState.guild.id,
+      },
+      handleUserAction
+    );
+    oldStateData = await fetchChannel(
+      client,
+      oldState.channelId,
+      {
+        redisClient: redisClient,
+        channelUsers: redisOldStateChannelUsers,
+        guildId: oldState.guild.id,
+      },
+      handleUserAction
+    );
+  });
+
+  return { newStateData: newStateData, oldStateData: oldStateData };
+};
+
 const handleUserAction = async (channel, data) => {
   const { channelUsers, redisClient, guildId } = data;
   const currentChannelUsers = getSetOfUsernameFromVC(channel);
@@ -50,40 +93,12 @@ const handleUserAction = async (channel, data) => {
 };
 
 module.exports = async (client, oldState, newState) => {
-  const { redisClient } = client;
 
   if (oldState.channelId === newState.channelId) {
     return;
   }
 
-  const redisNewStateChannelUsers = await redisClient.SMEMBERS(
-    `${newState.guild.id}:${newState.channelId}`
-  );
-
-  const redisOldStateChannelUsers = await redisClient.SMEMBERS(
-    `${oldState.guild.id}:${oldState.channelId}`
-  );
-
-  const newStateData = await fetchChannel(
-    client,
-    newState.channelId,
-    {
-      redisClient: redisClient,
-      channelUsers: redisNewStateChannelUsers,
-      guildId: newState.guild.id,
-    },
-    handleUserAction
-  );
-  const oldStateData = await fetchChannel(
-    client,
-    oldState.channelId,
-    {
-      redisClient: redisClient,
-      channelUsers: redisOldStateChannelUsers,
-      guildId: oldState.guild.id,
-    },
-    handleUserAction
-  );
+  const { oldStateData, newStateData } = await fetchRedisData(client,oldState,newState);
 
   try {
     if (oldState.channelId == null) {
