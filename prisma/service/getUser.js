ba835662ddb4eceea12857dcd1991797aa00a720
betester <ikramzulkarnaen02@gmail.com>
2022-12-23 21:20:09 +0700

    fix when the user has not been created yet on the database

--- prisma/service/getUser.js
+++ prisma/service/getUser.js
@@ -1,5 +1,10 @@
 const { prisma } = require("../index");
+const { saveUser } = require("./saveUser");
 const getUser = async (userId) => {
+  let user = await prisma.user.findFirst({ where: { username: userId } });
+  if (!user) {
+    await saveUser(userId, 0);
+  }
   return await prisma.user.findFirst({ where: { username: userId } });
 };
 
