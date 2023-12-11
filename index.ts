dc400576d56874c96b66f00c395b274d9adc29d9
betester <ikramzulkarnaen02@gmail.com>
2023-12-12 00:05:08 +0700

    add command boilerplate

--- index.ts
+++ index.ts
@@ -1,17 +1,30 @@
-import { Client, ClientOptions, GatewayIntentBits, VoiceState } from "discord.js"
+import { Client, ClientOptions, GatewayIntentBits, REST, Routes, VoiceState } from "discord.js"
 import jsLogger from "js-logger"
 import { voiceStateConsumer, consumeVoiceStateComplete } from "./consumer"
-import {  LocalStorage, MemoryCache, SpamFilterService, SpamFilterServiceImpl, UserDataService, UserDataServiceImpl, UserProfileService, UserProfileServiceImpl } from "./service"
+import {
+  LocalStorage,
+  MemoryCache,
+  SpamFilterService,
+  SpamFilterServiceImpl,
+  UserDataService,
+  UserDataServiceImpl,
+  UserProfileService,
+  UserProfileServiceImpl
+} from "./service"
 import { MemoryStorage } from "node-ts-cache-storage-memory"
-import {  UserProfile, UserSpam } from "./models"
+import { UserProfile, UserSpam } from "./models"
 import { MongoClient } from "mongodb"
 import { MongoUserDataServiceImpl } from "./service/mongoUserDataServiceImpl"
+import { commands } from "./commands"
 
 const main = () => {
   jsLogger.useDefaults()
 
   const Logger = jsLogger.get("main")
+
   const TOKEN = process.env.DISCORD_BOT_TOKEN
+  const CLIENT_ID = process.env.DISCORD_CLIENT_ID
+
   const SPAM_THRESHOLD = 5
   const TTL_SECONDS = 10
 
@@ -21,17 +34,17 @@ const main = () => {
 
   const memoryCache: MemoryStorage = new MemoryStorage()
   const cache: LocalStorage<UserSpam> = new MemoryCache<UserSpam>(memoryCache)
-  const mongoClient : MongoClient = new MongoClient(MONGO_URL)
+  const mongoClient: MongoClient = new MongoClient(MONGO_URL)
 
-  const userProfileDataService : UserDataService<UserProfile> = new MongoUserDataServiceImpl(
-    mongoClient, 
+  const userProfileDataService: UserDataService<UserProfile> = new MongoUserDataServiceImpl(
+    mongoClient,
     MONGO_DATABASE_NAME,
     MONGO_USER_PROFILE_COLLECTION_NAME
   )
-  const userDataService : UserDataService<UserSpam> = new UserDataServiceImpl(cache, TTL_SECONDS) 
+  const userDataService: UserDataService<UserSpam> = new UserDataServiceImpl(cache, TTL_SECONDS)
 
-  const spamFilterService : SpamFilterService = new SpamFilterServiceImpl(userDataService, SPAM_THRESHOLD)
-  const userProfileService : UserProfileService = new UserProfileServiceImpl(userProfileDataService)
+  const spamFilterService: SpamFilterService = new SpamFilterServiceImpl(userDataService, SPAM_THRESHOLD)
+  const userProfileService: UserProfileService = new UserProfileServiceImpl(userProfileDataService)
 
   Logger.info("Configuring discord bot...")
 
@@ -42,20 +55,38 @@ const main = () => {
       GatewayIntentBits.MessageContent
     ]
   }
+
   const client: Client = new Client(clientOptions)
 
   client.on("ready", () => {
     Logger.info("Bot is ready 🚀")
-    client.on("voiceStateUpdate", (oldVoiceChannelState: VoiceState, newVoiceChannelState: VoiceState) => {
-      voiceStateConsumer(client, oldVoiceChannelState, newVoiceChannelState, spamFilterService, userProfileService)
-    })
-    client.on("voiceStateComplete", (userId: string, guildId : string) => {
-      consumeVoiceStateComplete(userId, guildId, spamFilterService)
-    })
   })
 
-
+  client.on("voiceStateUpdate", (oldVoiceChannelState: VoiceState, newVoiceChannelState: VoiceState) => {
+    voiceStateConsumer(client, oldVoiceChannelState, newVoiceChannelState, spamFilterService, userProfileService)
+  })
+  client.on("voiceStateComplete", (userId: string, guildId: string) => {
+    consumeVoiceStateComplete(userId, guildId, spamFilterService)
+  })
   client.login(TOKEN)
+
+  Logger.info("Configuring commands...")
+
+  const rest: REST = new REST({ version: '10' }).setToken(TOKEN)
+  rest
+    .put(
+      Routes.applicationCommands(CLIENT_ID),
+      { body: commands }
+    )
+    .then( _ => {
+      Logger.info("Commands are set 🫡, here are the following commands")
+      commands.forEach(command => {
+        Logger.info(command)
+      })
+    })
+    .catch(error => {
+      Logger.error(error)
+    })
 }
 
 main()
