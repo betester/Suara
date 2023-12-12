26b7b6013f71c01f0ff4ad2517de04a3cd8b2b4d
betester <ikramzulkarnaen02@gmail.com>
2023-12-12 09:49:49 +0700

    handle unexpected case such as machine died

--- service/userProfileServiceImpl.ts
+++ service/userProfileServiceImpl.ts
@@ -1,3 +1,4 @@
+import { UserAction } from "../enums";
 import { UserProfile } from "../models";
 import { UserDataService } from "./userDataService";
 import { UserProfileService } from "./userProfileService";
@@ -15,15 +16,17 @@ export class UserProfileServiceImpl implements UserProfileService {
     this.userDataService = userDataService
   }
 
-  public save(userId: string) {
+  public save(userId: string, userAction: UserAction) {
     this.get(userId)
       .then(previousProfile => {
-        const userProfile: UserProfile = {
-          username: userId,
-          totalTimeSpent: previousProfile == null ? 0 : previousProfile.totalTimeSpent + Date.now() - previousProfile.lastTimeJoined,
-          lastTimeJoined: Date.now()
-        }
-        this.userDataService.save(userId, userProfile)
+        this.userDataService.save(
+          userId,
+          this.getUserProfile(
+            userId, 
+            previousProfile, 
+            userAction
+          )
+        )
       })
       .catch(error => {
         Logger.error(error)
@@ -37,4 +40,28 @@ export class UserProfileServiceImpl implements UserProfileService {
       return Promise.reject(error)
     }
   }
+
+  private getUserProfile(userId: string, previousProfile: UserProfile, currentAction: UserAction): UserProfile {
+
+    const currentTime: number = Date.now()
+    let newTotalTimeSpent = 0
+
+    if (previousProfile != null) {
+      const { lastTimeJoined, totalTimeSpent, lastUserAction } = previousProfile
+      newTotalTimeSpent = totalTimeSpent
+      if (lastUserAction == UserAction.JOIN && currentAction == UserAction.LEAVE) {
+        newTotalTimeSpent += currentTime - lastTimeJoined
+      }
+    }
+
+    const userProfile: UserProfile = {
+      username: userId,
+      lastTimeJoined: currentTime,
+      totalTimeSpent: newTotalTimeSpent,
+      lastUserAction: currentAction,
+    }
+
+    return userProfile
+  }
+
 }
