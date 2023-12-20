19db6d066858b1d91050e681969c121cab63a16f
betester <ikramzulkarnaen02@gmail.com>
2023-12-20 12:47:48 +0700

    add option for sselecting other user

--- commands/index.ts
+++ commands/index.ts
@@ -1,16 +1,34 @@
+import { ApplicationCommandOptionType } from "discord.js";
+
 export { Command } from "./command";
 export * from "./profile";
 
 export type CommandName = "profile";
 
+interface CommandOption {
+  name: string;
+  description: string;
+  type: ApplicationCommandOptionType;
+  required: boolean;
+}
+
 interface CommandDescription {
   name: CommandName;
   description: string;
+  options? : CommandOption[]
 }
 
 export const commands: CommandDescription[] = [
   {
     name: "profile",
     description: "Shows how much time have you spent on voice chat!",
+    options: [
+      {
+        name: "user",
+        description: "Select user",
+        type: ApplicationCommandOptionType.User,
+        required: false
+      }
+    ]
   },
 ];
