3e5b8d470269d65f8f42810bd25c0292a079acc8
betester <ikramzulkarnaen02@gmail.com>
2024-01-08 21:56:25 +0700

    add boilerplate for leaderboard, probably gonna continue tommorow :)

--- commands/leaderboard.ts
+++ commands/leaderboard.ts
@@ -1,36 +1,69 @@
-import {
-  CommandInteraction,
-  CacheType,
-  EmbedBuilder,
-  Client,
-  User,
-  APIEmbedField,
-} from "discord.js";
+import { CommandInteraction, CacheType, EmbedBuilder } from "discord.js";
 import { Command } from "./command";
-import { UserProfileService } from "../service";
+import { UserProfileService, TimeTogetherSpentService } from "../service";
 import jsLogger, { ILogger } from "js-logger";
-import utils from "../utils";
+import { UserProfile } from "../models";
 
 jsLogger.useDefaults();
 
 const Logger: ILogger = jsLogger.get("leaderboardCommand");
-const MAX_LIMIT = 10;
 
 export class LeaderboardCommand implements Command {
   private userProfileService: UserProfileService;
+  private timeTogetherSpentService: TimeTogetherSpentService;
 
-  constructor(userProfileService: UserProfileService) {
+  constructor(
+    userProfileService: UserProfileService,
+    timeTogetherSpentService: TimeTogetherSpentService,
+  ) {
     this.userProfileService = userProfileService;
+    this.timeTogetherSpentService = timeTogetherSpentService;
   }
 
   public async execute(interaction: CommandInteraction<CacheType>) {
-    // get all user that joins the voice channel
-    const currentlyJoiningUser =
-      await this.userProfileService.getCurrentUserInVoiceChannel(
-        interaction.guildId,
-      );
-    // update manually time spent with each other, consider async
-    // update manually for each user the time spent of individual
-    // return top N from the leaderboard
+    try {
+      // get all user that joins the voice channel
+      // at some point probably gonna need some rate limiter, this can be really slow if
+      // abused
+      const currentlyJoiningUser =
+        await this.userProfileService.getCurrentUserInVoiceChannel(
+          interaction.guildId,
+        );
+
+      // this steps is needed because if not, then the time of current user is updated
+      // and it won't be consistent for the next time user calling profile
+      for (let i = 0; i < currentlyJoiningUser.length; i++) {
+        for (let j = i + 1; j < currentlyJoiningUser.length; j++) {
+          this.timeTogetherSpentService.updateTimeSpentWith(
+            currentlyJoiningUser[i],
+            currentlyJoiningUser.splice(i + 1),
+          );
+        }
+      }
+
+      // if there are any user joining then it's necessary to update to newest
+      // condition for consistency
+      const savePromises: Promise<UserProfile>[] = [];
+      for (let i = 0; i < currentlyJoiningUser.length; i++) {
+        savePromises.push(
+          this.userProfileService.saveByUserAction(
+            currentlyJoiningUser[i].username,
+            interaction.guildId,
+            currentlyJoiningUser[i].lastUserAction,
+          ),
+        );
+      }
+      // return top N from the leaderboard
+      Promise.all(savePromises).then(async (_) => {
+        const topNusers = this.userProfileService.leaderboard(
+          10,
+          interaction.guildId,
+        );
+        const leaderboardEmbed = new EmbedBuilder();
+        //TODO: continue showing the embed
+      });
+    } catch (error) {
+      Logger.error(error);
+    }
   }
 }
